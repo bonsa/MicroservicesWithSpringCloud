a26579c8ac26422713fea9cb62b1e30c4ace1adf
Kasia Wasak <kasia.wasak@gmail.com>
2018-06-16 20:10:09 +0200

    lab5, Run Config Server, Eureka, and the word servers

---
+++ lab5/lab5-sentence-server/src/main/java/demo/controller/SentenceController.java
@@ -0,0 +1,43 @@
+package demo.controller;
+
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class SentenceController {
+
+	@Autowired
+	private DiscoveryClient client;
+
+	@GetMapping("/sentence")
+	public @ResponseBody
+	String getSentence() {
+		return
+				getWord("SUBJECT") + " "
+						+ getWord("VERB") + " "
+						+ getWord("ARTICLE") + " "
+						+ getWord("ADJECTIVE") + " "
+						+ getWord("NOUN") + "."
+				;
+	}
+
+	public String getWord(String service) {
+		List<ServiceInstance> list = client.getInstances(service);
+		if (list != null && list.size() > 0 ) {
+			URI uri = list.get(0).getUri();
+			if (uri !=null ) {
+				return (new RestTemplate()).getForObject(uri,String.class);
+			}
+		}
+		return null;
+	}
+
+}
