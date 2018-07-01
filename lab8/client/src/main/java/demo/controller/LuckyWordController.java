0173c88e01c48c5900212505763abdd89287abcd
Kasia Wasak <kasia.wasak@gmail.com>
2018-07-01 22:04:03 +0200

    lab8, BONUS, @RefreshScope

--- lab8/client/src/main/java/demo/controller/LuckyWordController.java
+++ lab8/client/src/main/java/demo/controller/LuckyWordController.java
@@ -1,19 +1,29 @@
 package demo.controller;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 @RestController
+@RefreshScope
 @ConfigurationProperties(prefix="wordConfig")
 public class LuckyWordController {
 
     String luckyWord;
     String preamble;
+    String fullStatement;
+
+    @PostConstruct
+    private void init() {
+        fullStatement = preamble + ": " + luckyWord;
+    }
 
     @GetMapping("/lucky-word")
     public String showLuckyWord() {
-        return preamble + ": " + luckyWord;
+        return fullStatement;
     }
 
     public String getLuckyWord() {
